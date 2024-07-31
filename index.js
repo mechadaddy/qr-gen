a11851091e213afcd5b4bbaa053131847c8c07b5
mechadaddy <164714633+mechadaddy@users.noreply.github.com>
2024-07-31 16:29:36 -0500

    develop interactibility

--- index.js
+++ index.js
@@ -1,26 +1,43 @@
-import inquirer from 'inquirer';
+import express from 'express';
+import bodyParser from 'body-parser';
 import QRCode from 'qrcode';
-import fs from 'fs';
+import path from 'path';
+import ejs from "ejs";
 
-var questions = [
-    {
-    type: "input",
-    name: "link",
-    message: "What is the link you want to qr-codify?"
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// Route to render the index.ejs page
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: "Qr Gen",
+    qrCodeUrl: null
+  });
+});
+
+// Route to handle form submission and QR code generation
+app.post('/generate', (req, res) => {
+  const url = req.body.url;
+
+  if (!url) {
+    return res.status(400).send('URL is required');
+  }
+
+  QRCode.toDataURL(url, (err, src) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Error generating QR code');
     }
-];
 
+    // Render the index.ejs page and pass the generated QR code URL
+    res.render('index', { title: 'QR Code Generated', qrCodeUrl: src });
+  });
+});
 
-inquirer.prompt(questions).then((answers) => {
-var saniUrl = answers.link.replace(/^(https?:\/\/)?(www\.)?|\.(com|net|org)$/g, '');
-var url = answers.link;
-var pathName = `./images/${saniUrl}.png`;
-    QRCode.toFile(pathName, url, {
-      }, function (err) {
-        if (err) throw err;
-      })
-      fs.appendFile('codes.txt', `\n${url}`, (err) => {
-        if (err) throw err;
-        console.log('Your image is saved, and the new line was appended to the file!');
-      });
-    });
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
